11e5aae1925184b073d347cb38dfd78e8653c492
ahmed baraka <Ahmed.gradion.com>
2026-04-22 16:50:51 +0200

    i changed the component inputs and buttons

--- components/atoms/button.tsx
+++ components/atoms/button.tsx
@@ -2,7 +2,7 @@ const SignInButton = ({ text }: { text: string }) => {
   return (
     <button
       type="submit"
-      className="w-[440px] max-w-full h-[48px] bg-[#FB7200] hover:bg-[#c44e0f] text-white text-[18px] font-bold rounded-[16px] transition-all cursor-pointer shadow-md active:scale-[0.98]"
+      className="w-[440px] max-w-full h-[50px] bg-[#FB7200] hover:bg-[#c44e0f] text-white text-[18px] font-bold rounded-[16px] transition-all cursor-pointer shadow-md active:scale-[0.98]"
     >
       {text}
     </button>
